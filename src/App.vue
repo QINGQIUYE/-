ad0211aca0e5fa8d9c2c3353f80a860e1cbd1168
QINGQIUYE <liqingxiang2018@gmail.com>
2019-07-18 22:08:57 +0800

    “修改说明”

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+   <router-view></router-view>
+   <van-tabbar v-model="active">
+  <van-tabbar-item icon="home-o">首页</van-tabbar-item>
+  <van-tabbar-item icon="search" dot>查询</van-tabbar-item>
+  <van-tabbar-item icon="friends-o" >联系人</van-tabbar-item>
+  <van-tabbar-item icon="setting-o" info="20">设置</van-tabbar-item>
+</van-tabbar>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {
+      active: 0
+    }
+  },
+}
+</script>
+
+<style>
+#app {
+ 
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="hello">
+大家好我是渣渣灰
+
+  </div>
+  
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  data() {
+    return {
+    
+    };
+  },
+};
+</script>
+
+
+<style scoped>
+</style>
